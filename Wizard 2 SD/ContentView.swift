4ec2c758c95f6a84c39e28752b09f8386c052390
Olivier Van hamme <nicefiction@gmail.com>
2021-08-01 10:51:48 +0200

    Initial Commit

---
+++ Wizard 2 SD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wizard 2 SD
+//
+//  Created by Olivier Van hamme on 01/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
